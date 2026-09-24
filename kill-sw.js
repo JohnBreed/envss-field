825d7512fc0ba8d58062bfe0448b4a74983bcfcc
JohnBreed <john@envss.com.au>
2026-09-24 17:08:17 +1000

    Unregister stale service workers so the header logo can load

---
+++ kill-sw.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (regs) {
+    regs.forEach(function (r) { r.unregister(); });
+  });
+}
+if (window.caches) {
+  caches.keys().then(function (keys) {
+    keys.forEach(function (k) { caches.delete(k); });
+  });
+}
