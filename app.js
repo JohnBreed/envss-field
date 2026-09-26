9ad8f674ad32ed95ec296253da56e4616807c079
JohnBreed <john@envss.com.au>
2026-09-26 20:14:34 +1000

    Fix blank screen: load hygiene core from jsDelivr with correct JS MIME

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-/* ENVSS Field v31 loader. Hygiene core from last good commit; AFM overlay in app-fibre.js. */
-document.write('<script src="https://raw.githubusercontent.com/JohnBreed/envss-field/138a583f7a75f3ba61024ec6c839080d8b46c33a/app.js"><\/script>');
-document.write('<script src="app-fibre.js?v=31"><\/script>');
+/* v32: index.html loads hygiene core from jsDelivr then app-fibre.js. This file is unused. */
